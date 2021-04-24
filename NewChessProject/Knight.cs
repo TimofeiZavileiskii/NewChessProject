fd496ee54b3b956bdadf96de4322eec4cc9ddd9a
Timofei Zavileiskii <K036829@eltham-college.org.uk>
2021-04-24 14:32:42 +0100

    Returned knight class

--- NewChessProject/Knight.cs
+++ NewChessProject/Knight.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewChessProject
+{
+    class Knight : OneStepPiece
+    {
+        public Knight(PlayerColour color, bool hasMoved = false) : base(color, hasMoved)
+        {
+            Type = PieceType.Knight;
+            directions.Add(new Vector(2, 1));
+            directions.Add(new Vector(2, -1));
+            directions.Add(new Vector(-2, 1));
+            directions.Add(new Vector(-2, -1));
+            directions.Add(new Vector(1, 2));
+            directions.Add(new Vector(1, -2));
+            directions.Add(new Vector(-1, -2));
+            directions.Add(new Vector(-1, 2));
+        }
+    }
+}
